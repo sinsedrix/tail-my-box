06095aad0673d7609fbce1ee4f9148b14b8706cf
Sinsedrix <sinsedrix@gmail.com>
2019-10-30 01:18:19 +0100

    optimize

--- src/components/boxparts/face.js
+++ src/components/boxparts/face.js
@@ -14,7 +14,7 @@ const Face = ({ id, oX, oY, w, h, dw, dh, nw, nh, bt, lt, dcut, flExtX, flExtY, 
     let dwin = dw - 2 * lt, dwou = dw + 2 * lt;
     let dhin = dh - 2 * lt, dhou = dh + 2 * lt;
     let btin = bt - 2 * lt;
-    let lines = [];
+    let linesB = [], linesT = [], linesL = [], linesR = [];
     let fdw = (flExt, idx) => { return flExt === (idx % 2 ? true : false) ? dwin : dwou; };
     let fdh = (flExt, idx) => { return flExt === (idx % 2 ? true : false) ? dhin : dhou; };
     let fbt = (flExt, idx) => { return btin * (flExt === (idx % 2 ? true : false)); };
@@ -23,11 +23,11 @@ const Face = ({ id, oX, oY, w, h, dw, dh, nw, nh, bt, lt, dcut, flExtX, flExtY, 
         let dwi = fdw(flExtX, i);
         let bti = fbt(flExtX, i);
 
-        lines.push({ x1: x, y1: bti, x2: x + dwi, y2: bti }); //-
-        if (i < 2 * nw) lines.push({ x1: x + dwi, y1: 0, x2: x + dwi, y2: btin }); //|
+        linesB.push({ x1: x, y1: bti, x2: x + dwi, y2: bti }); //-
+        if (i < 2 * nw) linesB.push({ x1: x + dwi, y1: 0, x2: x + dwi, y2: btin }); //|
 
-        lines.push({ x1: x, y1: h - bti, x2: x + dwi, y2: h - bti }); //-
-        if (i < 2 * nw) lines.push({ x1: x + dwi, y1: h, x2: x + dwi, y2: h - btin }); //|
+        linesT.push({ x1: x, y1: h - bti, x2: x + dwi, y2: h - bti }); //-
+        if (i < 2 * nw) linesT.push({ x1: x + dwi, y1: h, x2: x + dwi, y2: h - btin }); //|
         x += dwi;
     }
 
@@ -35,14 +35,15 @@ const Face = ({ id, oX, oY, w, h, dw, dh, nw, nh, bt, lt, dcut, flExtX, flExtY, 
         let dhj = fdh(flExtY, j);
         let btj = fbt(flExtY, j);
 
-        lines.push({ x1: btj, y1: y, x2: btj, y2: y + dhj }); //-
-        if (j < 2 * nh) lines.push({ x1: 0, y1: y + dhj, x2: btin, y2: y + dhj }); //|
+        linesL.push({ x1: btj, y1: y, x2: btj, y2: y + dhj }); //-
+        if (j < 2 * nh) linesL.push({ x1: 0, y1: y + dhj, x2: btin, y2: y + dhj }); //|
 
-        lines.push({ x1: w - btj, y1: y, x2: w - btj, y2: y + dhj }); //-
-        if (j < 2 * nh) lines.push({ x1: w, y1: y + dhj, x2: w - btin, y2: y + dhj }); //|
+        linesR.push({ x1: w - btj, y1: y, x2: w - btj, y2: y + dhj }); //-
+        if (j < 2 * nh) linesR.push({ x1: w, y1: y + dhj, x2: w - btin, y2: y + dhj }); //|
         y += dhj;
     }
 
+    let lines = linesL.concat(linesR).concat(linesB).concat(linesT);
     if (dcut) lines.push({ x1: btin, y1: dcut, x2: w - btin, y2: dcut });
 
     return (
