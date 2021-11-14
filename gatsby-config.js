2cbbcfb2ec5878e40d8b2c434f8d54dc89079326
Sinsedrix <sinsedrix@gmail.com>
2021-11-14 18:59:30 +0100

    maj pkg

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   pathPrefix: '/tail-my-box',
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -28,9 +29,6 @@ module.exports = {
         icon: `src/images/logo-w.png`, // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-sass',
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+    'gatsby-plugin-sass'
+  ]
 }
